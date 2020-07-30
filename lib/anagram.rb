9a1435b03c1227f665a994eb79e1826e2df0db29
Ebo Lee <yiboli.coding@gmail.com>
2020-07-30 02:05:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+class Anagram
+  attr_accessor
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def self.match
+    
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
